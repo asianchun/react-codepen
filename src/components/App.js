6a66c9a8aaed85a340e486c1fccd843af8b47d35
HongYuYi <1chunchun578@gmail.com>
2022-07-20 20:00:22 +1000

    HTML setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,24 @@
+import Editor from "./Editor"
+
 function App() {
-  return "Hello"
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts" //For security reasons
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  )
 }
 
 export default App
